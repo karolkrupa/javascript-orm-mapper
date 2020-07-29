c70770e01462556b58001aaf3d54360b7cae8f83
Karol Krupa <karo2krupa@gmail.com>
2020-07-29 19:07:25 +0200

    OneToOne and ManyToMany relation

---
+++ src/DataType/OneToOne.ts
@@ -0,0 +1,36 @@
+import {Type} from "./Type";
+import MappingHelper from "../Mapper/MappingHelper";
+import database from "../Database";
+import ModelMapper from "../Mapper/ModelMapper";
+
+export class OneToOne extends Type {
+    private entityName: string = null
+
+    constructor(modelName: string) {
+        super();
+
+        this.entityName = modelName
+    }
+
+    public normalize(data: any): any {
+        let model = database.getModel(this.entityName);
+
+        let idField = MappingHelper.getObjectIdFieldName(model)
+
+        if(!data[idField]) return null;
+
+        let entity = database.get(model, data[idField])
+
+        if(!entity) {
+            entity = ModelMapper.persist(data, model)
+        }
+
+        return entity;
+    }
+}
+
+export default (entityName: string): PropertyDecorator => {
+    let type = new OneToOne(entityName)
+
+    return MappingHelper.getTypeDecorator(type)
+}
